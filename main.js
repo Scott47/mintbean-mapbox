109ab8173281153c95a008fcbf6b2cbb25217ff3
scottsilver <scottsilver47@gmail.com>
2020-06-15 12:10:11 -0500

    connected to mapbox

---
+++ main.js
@@ -0,0 +1,16 @@
+const ACCESS_TOKEN = 'pk.eyJ1IjoiYXRvbWljbm80NyIsImEiOiJja2Jnb3c5N3QxMGJtMnhudWlveWJxM2FtIn0.teti1uOp-ljFqawM8tehoQ';
+const MAPBOX_STYLE = 'mapbox://styles/atomicno47/ckbgqpw5z0nin1io16h47uk2c';
+
+
+mapboxgl.accessToken = ACCESS_TOKEN;
+
+var map = new mapboxgl.Map({
+  container: 'map',
+
+
+  style: 'mapbox://styles/atomicno47/ckbgqpw5z0nin1io16h47uk2c',
+  center: [-77.04, 38.907],
+  zoom: 14
+});
+
+
